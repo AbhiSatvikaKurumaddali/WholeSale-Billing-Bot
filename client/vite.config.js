7a833b4e6bea3b47a56b7e765ed51809a7cce5eb
AbhiSatvikaKurumaddali <kurumaddali1201@gmail.com>
2026-06-12 17:06:18 +0530

    Add files via upload

---
+++ client/vite.config.js
@@ -0,0 +1,32 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+// https://vite.dev/config/
+/** @type {import('tailwindcss').Config} */
+export default defineConfig({
+   /** @type {import('tailwindcss').Config} */
+  darkMode: 'class',
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        gray: {
+          50: '#f9fafb',
+          100: '#f3f4f6',
+          200: '#e5e7eb',
+          300: '#d1d5db',
+          400: '#9ca3af',
+          500: '#6b7280',
+          600: '#4b5563',
+          700: '#374151',
+          800: '#1f2937',
+          900: '#111827',
+        }
+      }
+    },
+  },
+  plugins: [react(),tailwindcss()],
+})
